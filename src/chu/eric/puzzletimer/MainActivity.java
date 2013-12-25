3954180fca37f32bca7c801d5fa7646a2c82e381
Eric Chu <ericchu94@gmail.com>
2013-12-24 19:26:24 -0800

    Initial implementation

--- src/chu/eric/puzzletimer/MainActivity.java
+++ src/chu/eric/puzzletimer/MainActivity.java
@@ -1,11 +1,19 @@
 package chu.eric.puzzletimer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Locale;
 import java.util.Random;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -14,7 +22,11 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements
@@ -101,6 +113,9 @@ public class MainActivity extends FragmentActivity implements
 			FragmentTransaction fragmentTransaction) {
 	}
 
+	private Fragment timerFragment = new TimerFragment();
+	private HistoryFragment historyFragment = new HistoryFragment();
+
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
@@ -113,17 +128,13 @@ public class MainActivity extends FragmentActivity implements
 
 		@Override
 		public Fragment getItem(int position) {
-			Fragment fragment = null;
 			switch (position) {
 			case 0:
-
-				fragment = new TimerFragment();
-				break;
+				return timerFragment;
 			case 1:
-				fragment = new HistoryFragment();
-				break;
+				return historyFragment;
 			}
-			return fragment;
+			return null;
 		}
 
 		@Override
@@ -145,25 +156,163 @@ public class MainActivity extends FragmentActivity implements
 		}
 	}
 
-	public static class TimerFragment extends Fragment {
+	public static class TimerFragment extends Fragment implements
+			OnClickListener {
+
+		enum State {
+			Idle, Solving
+		}
+
+		class AccelerometerSensorEventListener implements SensorEventListener {
+
+			class SensorValue {
+				private long timestamp;
+				private double value;
+
+				public SensorValue(double value) {
+					timestamp = System.currentTimeMillis();
+					this.value = value;
+				}
+
+				public long getTimestamp() {
+					return timestamp;
+				}
+
+				public double getValue() {
+					return value;
+				}
+			}
+
+			List<SensorValue> sensorValues = new ArrayList<SensorValue>();
+
+			@Override
+			public void onAccuracyChanged(Sensor arg0, int arg1) {
+				// TODO Auto-generated method stub
+
+			}
+
+			private final int contactDuration = 200;
+
+			public double getChange() {
+				long start = System.currentTimeMillis() - contactDuration;
+				double min = Double.MAX_VALUE;
+				double max = Double.MIN_VALUE;
+
+				ListIterator<SensorValue> iterator = sensorValues
+						.listIterator();
+				while (iterator.hasNext()) {
+					SensorValue sensorValue = iterator.next();
+					if (sensorValue.getTimestamp() < start) {
+						iterator.remove();
+					} else {
+						double value = sensorValue.getValue();
+						if (value < min)
+							min = value;
+						else if (value > max)
+							max = value;
+					}
+				}
+
+				return max - min;
+			}
+
+			private static final float changeThreshhold = 0.5f;
+
+			@Override
+			public void onSensorChanged(SensorEvent event) {
+				// TODO Auto-generated method stub
+				double acceleration = getAcceleration(event.values[0],
+						event.values[1], event.values[2]);
+				sensorValues.add(new SensorValue(acceleration));
+
+				if (getChange() > changeThreshhold) {
+					timer.performClick();
+				}
+
+			}
+
+			private double getAcceleration(float x, float y, float z) {
+				return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)
+						+ Math.pow(z, 2));
+			}
+		}
+
+		private SensorManager sManager;
+		private Sensor accelerometer;
+
+		private AccelerometerSensorEventListener accelerometerSensorEventListener = new AccelerometerSensorEventListener();
 
 		private Scrambler scrambler = new RubiksCubeScrambler();
+		private long start;
+		private State state = State.Idle;
 		private Random random = new Random();
+		private Handler handler = new Handler();
+		private Runnable timerRunnable = new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				if (state == State.Solving) {
+					updateText(getDuration());
+					handler.postDelayed(timerRunnable, 0);
+				}
+			}
+		};
+		private Button timer;
+		private TextView scramble;
 
 		public TimerFragment() {
+		}
+
+		private float getDuration() {
+			return (System.currentTimeMillis() - start) / 1000f;
+		}
+
+		private void updateText(float text) {
+			timer.setText(String.format("%.2f", text));
 		}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
+			View rootView = inflater.inflate(R.layout.fragment_main_timer,
 					container, false);
 
-			// TODO: Init here
-			TextView scramble = (TextView) rootView.findViewById(R.id.scramble);
+			scramble = (TextView) rootView.findViewById(R.id.scramble);
 			scramble.setText(scrambler.generateScramble(random));
 
+			sManager = (SensorManager) this.getActivity().getSystemService(
+					SENSOR_SERVICE);
+			accelerometer = sManager
+					.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+			timer = (Button) rootView.findViewById(R.id.timer);
+			timer.setOnClickListener(this);
+
 			return rootView;
+		}
+
+		@Override
+		public void onClick(View view) {
+			float duration = getDuration();
+			switch (state) {
+			case Idle:
+				state = State.Solving;
+				start = System.currentTimeMillis();
+				handler.postDelayed(timerRunnable, 0);
+				sManager.registerListener(accelerometerSensorEventListener,
+						accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+				break;
+			case Solving:
+				state = State.Idle;
+				sManager.unregisterListener(accelerometerSensorEventListener);
+
+				((MainActivity) getActivity()).historyFragment
+						.addSolve(duration);
+				updateText(duration);
+
+				scramble.setText(scrambler.generateScramble(random));
+				break;
+			}
 		}
 	}
 
@@ -172,15 +321,28 @@ public class MainActivity extends FragmentActivity implements
 		public HistoryFragment() {
 		}
 
+		List<String> solves = new ArrayList<String>();
+		private ArrayAdapter<String> adapter;
+
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
+			View rootView = inflater.inflate(R.layout.fragment_main_history,
 					container, false);
 
-			// TODO: Init here
+			ListView solves = (ListView) rootView.findViewById(R.id.solves);
+			adapter = new ArrayAdapter<String>(getActivity(),
+					android.R.layout.simple_list_item_1, android.R.id.text1,
+					this.solves);
+			solves.setAdapter(adapter);
 
 			return rootView;
+		}
+
+		public void addSolve(float duration) {
+			// TODO: Accept scramble. Maybe create a solve class, etc
+			solves.add(String.format("%.2f", duration));
+			adapter.notifyDataSetChanged();
 		}
 	}
 }
