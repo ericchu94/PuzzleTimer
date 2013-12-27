8e606b0ae8ad58807629b51ea05c8fdf2679292e
Eric Chu <ericchu94@gmail.com>
2013-12-27 01:55:25 -0800

    Added name to preference summary

--- src/chu/eric/puzzletimer/SettingsActivity.java
+++ src/chu/eric/puzzletimer/SettingsActivity.java
@@ -1,9 +1,13 @@
 package chu.eric.puzzletimer;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements
+		OnSharedPreferenceChangeListener {
 
 	public static final String PREF_NAME = "pref_name";
 	public static final String PREF_ACCELEROMETER = "pref_accelerometer";
@@ -12,5 +16,38 @@ public class SettingsActivity extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+
+		updateEditTextPreferenceSummary(PREF_NAME);
 	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if (key.equals(PREF_NAME)) {
+			updateEditTextPreferenceSummary(key);
+		}
+
+	}
+
+	private void updateEditTextPreferenceSummary(String key) {
+		EditTextPreference preference = (EditTextPreference) findPreference(key);
+		preference.setSummary(preference.getText());
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		getPreferenceScreen().getSharedPreferences()
+				.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		getPreferenceScreen().getSharedPreferences()
+				.unregisterOnSharedPreferenceChangeListener(this);
+	}
+
 }
